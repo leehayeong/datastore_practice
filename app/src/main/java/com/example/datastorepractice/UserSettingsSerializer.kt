fd41cfdd9b7f0c85dc9430fb7b182397a2eaf410
leehayeong <lhaayyy@naver.com>
2022-01-18 20:19:12 +0900

    [6] proto serializer 생성

---
+++ app/src/main/java/com/example/datastorepractice/UserSettingsSerializer.kt
@@ -0,0 +1,28 @@
+package com.example.datastorepractice
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+/**
+ *  UserSettingsSerializer.kt.kt
+ *
+ *  Created by Hayeong Lee on 2022/01/18
+ *  Copyright © 2021 Shinhan Bank. All rights reserved.
+ */
+
+object UserSettingsSerializer : Serializer<UserSettings> {
+    override val defaultValue: UserSettings = UserSettings.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): UserSettings {
+        try {
+            return UserSettings.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(t: UserSettings, output: OutputStream) = t.writeTo(output)
+}
