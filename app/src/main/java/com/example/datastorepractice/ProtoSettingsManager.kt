0498e9e763e13377f01405f627b20e1ed3e565c0
leehayeong <lhaayyy@naver.com>
2022-01-20 16:08:06 +0900

    [9] proto datastore 읽고 쓰는 함수 구현
    - 읽기(getColor): datastore.data
    - 쓰기(updateColor): datastore.updateData

--- app/src/main/java/com/example/datastorepractice/ProtoSettingsManager.kt
+++ app/src/main/java/com/example/datastorepractice/ProtoSettingsManager.kt
@@ -3,6 +3,9 @@ package com.example.datastorepractice
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.dataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import java.io.IOException
 
 /**
  *  ProtoSettingsManager.kt.kt
@@ -16,4 +19,25 @@ class ProtoSettingsManager(private val context: Context) {
         fileName = "user_settings.pb",
         serializer = ProtoSettingsSerializer
     )
+
+    // 읽기
+    fun getColor(): Flow<UserSettings> {
+        return context.protoDataStore.data
+            .catch { ex ->
+                if (ex is IOException) {
+                    emit(UserSettings.getDefaultInstance())
+                } else {
+                    throw ex
+                }
+            }
+    }
+
+    // 쓰기
+    suspend fun updateColor(bgColor: UserSettings.BgColor) {
+        context.protoDataStore.updateData { userSettings ->
+            userSettings.toBuilder()
+                .setBgColor(bgColor)
+                .build()
+        }
+    }
 }
